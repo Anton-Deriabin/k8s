61cf94f44f4d6dabd2ae11e87d394ae4e617c96c
Антон Дерябин <toh309@yandex.ru>
2025-04-04 08:05:40 +0300

    ver 2 two deployments with nginx

--- src/main/java/ru/practicum/HomeController.java
+++ src/main/java/ru/practicum/HomeController.java
@@ -1,17 +1,19 @@
 package ru.practicum;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.InetAddress;
+import java.net.URI;
 import java.net.UnknownHostException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @RestController
-@RequestMapping("/")
 public class HomeController {
 
-    @GetMapping
+    @GetMapping("/")
     public String homePage() {
         String hostname = "unknown";
         try {
@@ -19,10 +21,24 @@ public class HomeController {
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
-
-        String message = "<h1>VERSION 2: Hello from the " + hostname + "</h1>";
-        System.out.println(message);
-
+        String message = "<h1>Hello from the " + hostname + "</h1>";
         return message;
     }
+
+    @GetMapping("/nginx")
+    public String callNginx() {
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI("http://nginx"))
+                    .GET()
+                    .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return response.body();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Ошибка при обращении к nginx: " + e.getMessage();
+        }
+    }
 }
+
